f4368e8a9046b75df596973d6715f37dcd0b548e
Vanja Stanic <epiller@users.noreply.github.com>
2024-08-23 15:31:17 +0700

    Updated Kuando BusyLight Downlink Converter
    This converter uses the upcoming feature of AllThingsTalk platform where the Downlink Port and Confirmed Downlink can be defined inside the conversion.

--- Plenom/Kuando BusyLight IoT Omega /JS Converters/Downlink Converter.js
+++ Plenom/Kuando BusyLight IoT Omega /JS Converters/Downlink Converter.js
@@ -1,5 +1,6 @@
 function converter(code) {
-    var given_obj = JSON.parse(code);
+    var att_in = JSON.parse(code);
+    var att_out = new Object();
 
     function decToHex(integer) {
         var shouldAddZero = arguments.length <= 1 || arguments[1] === undefined ? true : arguments[1];
@@ -11,51 +12,53 @@ function converter(code) {
         return number;
     };
 
-    if ('color' in given_obj) {
-        var hexR = decToHex(given_obj.color.value.r)
-        var hexG = decToHex(given_obj.color.value.g)
-        var hexB = decToHex(given_obj.color.value.b)
-        var hexOnTime = decToHex(given_obj.color.value.on_time);
-        var hexOffTime = decToHex(given_obj.color.value.off_time)
+    if ('color' in att_in) {
+        var hexR = decToHex(att_in.color.value.r)
+        var hexG = decToHex(att_in.color.value.g)
+        var hexB = decToHex(att_in.color.value.b)
+        // When using the color picker on the pinboard, it doesn't send the values below, so check if they're being sent and if not, use the defaults.
+        var hexOnTime = decToHex(typeof att_in.color.value.on_time !== 'undefined' ? att_in.color.value.on_time : 255)
+        var hexOffTime = decToHex(typeof att_in.color.value.off_time !== 'undefined' ? att_in.color.value.off_time : 0)
 
-        return hexR + hexB + hexG + hexOnTime + hexOffTime; // Yes, it's RBG and not RGB.
+        att_out.data = hexR + hexB + hexG + hexOnTime + hexOffTime;
     }
 
-    if ('factory_reset' in given_obj) {
-        if (given_obj.factory_reset.value == 'true' || given_obj.factory_reset.value == true) {
-            return 'AA00';
+    if ('factory_reset' in att_in) {
+        if (att_in.factory_reset.value == 'true' || att_in.factory_reset.value == true) {
+            att_out.data = 'AA00';
         }
     }
 
-    if ('adr' in given_obj) {
-        if (given_obj.adr.value == 'true' || given_obj.adr.value == true) {
-            return '0100';
-        } else if (given_obj.adr.value == 'false' || given_obj.adr.value == false) {
-            return '0200';
+    if ('adr' in att_in) {
+        if (att_in.adr.value == 'true' || att_in.adr.value == true) {
+            att_out.data = '0100';
+        } else if (att_in.adr.value == 'false' || att_in.adr.value == false) {
+            att_out.data = '0200';
         }
     }
 
-    if ('uplinks_without_downlink' in given_obj) {
-        return '03' + decToHex(given_obj.uplinks_without_downlink.value);
+    if ('uplinks_without_downlink' in att_in) {
+        att_out.data = '03' + decToHex(att_in.uplinks_without_downlink.value);
     }
 
-    if ('uplink_interval' in given_obj) {
-        return '04' + decToHex(given_obj.uplink_interval.value);
+    if ('uplink_interval' in att_in) {
+        att_out.data = '04' + decToHex(att_in.uplink_interval.value);
     }
 
-    if ('request_keepalive' in given_obj) {
-        if (given_obj.request_keepalive.value == 'true' || given_obj.request_keepalive.value == true) {
-            return '0500';
+    if ('request_keepalive' in att_in) {
+        if (att_in.request_keepalive.value == 'true' || att_in.request_keepalive.value == true) {
+            att_out.data = '0500';
         }
     }
     
-    if ('auto_uplink' in given_obj) {
-        if (given_obj.auto_uplink.value == 'true' || given_obj.auto_uplink.value == true) {
-            return '0601';
-        } else if (given_obj.auto_uplink.value == 'false' || given_obj.auto_uplink.value == false) {
-            return '0600';
+    if ('auto_uplink' in att_in) {
+        if (att_in.auto_uplink.value == 'true' || att_in.auto_uplink.value == true) {
+            att_out.data = '0601';
+        } else if (att_in.auto_uplink.value == 'false' || att_in.auto_uplink.value == false) {
+            att_out.data = '0600';
         }
     }
 
-    return "";
+    att_out.meta = { confirmed: true, port: 15 };
+    return JSON.stringify(att_out);
 }
