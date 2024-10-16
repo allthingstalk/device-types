c0fd2f630f8b3ac3d657796a78658aacb63fa567
Vanja Stanic <epiller@users.noreply.github.com>
2024-10-16 15:13:28 +0200

    Updated Watteco Smart Plug Downlink converter
    Added downlink port and fixed plug control

--- Watteco/Smart Plug/Watteco Smart Plug Downlink JS Converter.js
+++ Watteco/Smart Plug/Watteco Smart Plug Downlink JS Converter.js
@@ -1,22 +1,22 @@
 function converter(code) {
-    var given_obj = JSON.parse(code);
-    if ('Output' in given_obj) {
-        var value;
-        if (given_obj.Output.value === "true") {
-            value = 0x00;
+    var att_in = JSON.parse(code);
+    var att_out = new Object();
+    if ('Output' in att_in) {
+        if (att_in.Output.value == true || att_in.Output.value == 'true') {
+            att_out.data = '1150000601';
+
         }
-        if (given_obj.Output.value === "false") {
-            value = 0x01;
+        if (att_in.Output.value == false || att_in.Output.value == 'false') {
+            att_out.data = '1150000600';
         }
         // // This feature isn't used as it's better to have a boolean asset instead of a string one, but it's there.
         // if (given_obj.Output.value == "TOGGLE") {
-        //     value = 0x02;
+        //     att_out.data = '1150000602';
         // }
-        var bytes = [0x11, 0x50, 0x00, 0x06, value];
-        return bytes.toString(16);
     }
-    if ('RawDownlink' in given_obj) {
-        return given_obj.RawDownlink.value;
+    if ('RawDownlink' in att_in) {
+        att_out.data = att_in.RawDownlink.value;
     }
-    return "Error";
+    att_out.meta = { confirmed: true, port: 125 };
+    return JSON.stringify(att_out);
 }
